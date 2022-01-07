0109584b549d3d6628ba699b56565243902207e9
Peter Wang <wangpeter588@gmail.com>
2022-01-08 02:01:04 +0800

    20220108_finsh_origincod_week22_react-blog

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,7 +26,7 @@ function App() {
       <AuthContext.Provider value={{user, loading, error}}>
         <ThemeProvider theme={themeMode}>
           <Global styles={{body: {background: themeMode.background.body}}} />
-          <Router>
+          <Router "/react-blog"/>
             <Header/>
               <Routes>
                 <Route exact path='/' element={<LoginPage />} />
